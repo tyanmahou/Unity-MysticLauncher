09f4cd5ff576f843edeef9bd6ee2d5369d6a967a
tyanmahou <tyanmahou@gmail.com>
2024-08-24 06:21:03 +0900

    add folder element

---
+++ Assets/MysticLauncher/Editor/Element/FolderElement.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
+
+namespace Mystic
+{
+    [Serializable]
+    public class FolderElement : IElement
+    {
+        [FolderSelect]
+        public string Path;
+
+        public void OnGUI()
+        {
+            bool old = GUI.enabled;
+            using var horizontal = new EditorGUILayout.HorizontalScope();
+            var skin = new GUIStyle(EditorStyles.objectField);
+            GUILayout.Label(Path, skin);
+            var path = System.IO.Path.IsPathRooted(Path) 
+                ? Path
+                : Application.dataPath + "/../" + Path;
+            GUI.enabled = old && System.IO.Directory.Exists(path);
+            // フォルダを開く
+            {
+                GUIContent icon = EditorGUIUtility.IconContent("d_FolderOpened Icon");
+                if (GUILayout.Button(icon, GUILayout.Width(30), GUILayout.Height(EditorGUIUtility.singleLineHeight)))
+                {
+                    OpenFolder(path);
+                }
+            }
+            // ターミナルを開く
+            {
+                GUIContent icon = EditorGUIUtility.IconContent("d_BuildSettings.Standalone");
+                if (GUILayout.Button(icon, GUILayout.Width(30), GUILayout.Height(EditorGUIUtility.singleLineHeight)))
+                {
+                    OpenTerminal(path);
+                }
+            }
+            GUI.enabled = old;
+        }
+        void OpenFolder(string path)
+        {
+            try
+            {
+                using Process process = System.Diagnostics.Process.Start(path);
+            }
+            catch (System.Exception e)
+            {
+                UnityEngine.Debug.LogError(e.Message);
+            }
+        }
+        // ターミナル開く
+        void OpenTerminal(string path)
+        {
+            var terminalPath = LauncherPreferenceSettings.instance.TerminalPath;
+            if (!File.Exists(terminalPath))
+            {
+                terminalPath = string.Empty;
+            }
+            ProcessStartInfo processInfo = new ProcessStartInfo
+            {
+                FileName = terminalPath,
+                WorkingDirectory = path
+            };
+            try
+            {
+                using Process process = Process.Start(processInfo);
+            }
+            catch (System.Exception e)
+            {
+                UnityEngine.Debug.LogError(e.Message);
+            }
+        }
+        public override string ToString()
+        {
+            return Path;
+        }
+    }
+}
