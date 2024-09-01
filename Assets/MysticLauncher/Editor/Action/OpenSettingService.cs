f8f5fc6dca7cfbc746aead465f29c2441f21da81
tyanmahou <tyanmahou@gmail.com>
2024-09-01 21:34:56 +0900

    update SettingService

---
+++ Assets/MysticLauncher/Editor/Action/OpenSettingService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEditor;
+
+namespace Mystic
+{
+    [Serializable]
+    public class OpenSettingService : IToolAction
+    {
+        public SettingsScope Scope;
+        public string SettingPath;
+
+        public void Execute()
+        {
+            if (Scope == SettingsScope.Project)
+            {
+                SettingsService.OpenProjectSettings(SettingPath);
+            }
+            else
+            {
+                SettingsService.OpenUserPreferences(SettingPath);
+            }
+        }
+    }
+}
