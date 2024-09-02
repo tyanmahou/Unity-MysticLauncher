e8811a36c497d609849eedef60e601f2472255de
tyanmahou <tyanmahou@gmail.com>
2024-09-02 21:18:41 +0900

    add SettingServicePath

---
+++ Assets/MysticLauncher/Editor/Common/SettingServicePath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEditor;
+
+namespace Mystic
+{
+    /// <summary>
+    /// セッティングパス
+    /// </summary>
+    [Serializable]
+    public struct SettingServicePath
+    {
+        public SettingsScope Scope;
+        public string SettingPath;
+    }
+}
