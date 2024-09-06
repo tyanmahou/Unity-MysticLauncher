56d0b7e3fe075dcddc6255e8d30c440213c678c2
tyanmahou <tyanmahou@gmail.com>
2024-09-06 09:26:48 +0900

    add doubleclick

---
+++ Assets/MysticLauncher/Editor/Common/DoubleClick.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Mystic
+{
+    public class DoubleClickCtrl
+    {
+        public bool DoubleClick()
+        {
+            bool isDoubleClick = false;
+            double currentTime = EditorApplication.timeSinceStartup;
+            if (currentTime - _lastClickTime < doubleClickTime)
+            {
+                isDoubleClick = true;
+            }
+            _lastClickTime = currentTime;
+            return isDoubleClick;
+        }
+        private const float doubleClickTime = 0.3f;
+        private double _lastClickTime = 0;
+    }
+}
