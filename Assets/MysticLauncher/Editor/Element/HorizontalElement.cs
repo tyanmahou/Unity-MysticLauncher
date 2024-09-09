320e1ea85acc3f4efba4584e96d5debba45ab7c6
tyanmahou <tyanmahou@gmail.com>
2024-09-09 23:28:43 +0900

    add horizontal

---
+++ Assets/MysticLauncher/Editor/Element/HorizontalElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Mystic
+{
+    [Serializable]
+    public class HorizontalElement : IElement
+    {
+        [NamedArrayElement, SerializeReference, SubclassSelector]
+        public IElement[] Elements;
+
+        public void OnGUI()
+        {
+            using var horizontal = new EditorGUILayout.HorizontalScope();
+            foreach (var entry in Elements)
+            {
+                entry?.OnGUI();
+            }
+        }
+        public override string ToString()
+        {
+            return "Horizontal";
+        }
+    }
+}
