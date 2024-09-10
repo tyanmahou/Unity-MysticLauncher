e0b66bf8999dafb6fa316ceb38892a6b49e3c943
tyanmahou <tyanmahou@gmail.com>
2024-09-10 22:14:54 +0900

    add text element

---
+++ Assets/MysticLauncher/Editor/Element/TextElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Mystic
+{
+    [Serializable]
+    public class TextElement : IElement
+    {
+        [TextArea]
+        public string Text = string.Empty;
+
+        public void OnGUI()
+        {
+            GUIStyle labelStyle = new GUIStyle(GUI.skin.label);
+            labelStyle.wordWrap = true;
+            EditorGUILayout.LabelField(Text, labelStyle);
+        }
+        public override string ToString()
+        {
+            return Text.Split('\n')[0];
+        }
+    }
+}
