1ec31ef651e7f584bd83e2845ffc20bbfd1c7e2b
tyanmahou <tyanmahou@gmail.com>
2024-09-12 02:01:06 +0900

    update Category Element

--- Assets/MysticLauncher/Editor/Element/CategoryElement.cs
+++ Assets/MysticLauncher/Editor/Element/CategoryElement.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using UnityEditor;
 using UnityEngine;
+using UnityEditor.AnimatedValues;
 
 namespace Mystic
 {
     [Serializable]
     public class CategoryElement : IElement
     {
+        const float height = 30;
+        const float iconSize = 20;
+
         public Label Label;
 
         [NamedArrayElement, SerializeReference, SubclassSelector]
@@ -14,21 +18,70 @@ namespace Mystic
 
         public void OnGUI()
         {
-            const float height = 30;
             GUILayout.Space(4);
+            DrawHeader();
+
+            _fade.target = _isOpen;
+            if (EditorGUILayout.BeginFadeGroup(_fade.faded))
             {
-                var style = new GUIStyle(EditorStyles.helpBox);
-                style.margin.left += EditorGUI.indentLevel * 15;
-                using var skin = new EditorGUILayout.VerticalScope(style);
-                using var horizontal = new EditorGUILayout.HorizontalScope();
-                if (Label.Icon.TryGetGUIContent(out var content))
-                {
-                    Rect iconRect = GUILayoutUtility.GetRect(height, height, GUILayout.Width(height), GUILayout.Height(height));
-                    GUI.DrawTexture(iconRect, content.image, ScaleMode.ScaleToFit);
-                }
-                GUILayout.Label(Label.Text, EditorStyles.boldLabel, GUILayout.Height(height));
+                GUILayout.Space(2);
+                DrawContent();
             }
-            GUILayout.Space(2);
+            EditorGUILayout.EndFadeGroup();
+        }
+        private void DrawHeader()
+        {
+            var style = new GUIStyle(EditorStyles.helpBox);
+            style.margin.left += EditorGUI.indentLevel * 15;
+            Rect buttonRect = GUILayoutUtility.GetRect(0, height, style);
+
+            if (buttonRect.Contains(Event.current.mousePosition))
+            {
+                EditorGUI.DrawRect(buttonRect, new Color(0, 1, 1, 0.1f));
+            }
+
+            Rect contentRect = buttonRect;
+            contentRect.x += 5;
+            contentRect.y += 5;
+            contentRect.height = iconSize;
+
+            if (Label.Icon.TryGetGUIContent(out var content))
+            {
+                contentRect.width = iconSize;
+                GUI.DrawTexture(contentRect, content.image, ScaleMode.ScaleToFit);
+
+                contentRect.x += contentRect.width + 5;
+                contentRect.width = buttonRect.width - contentRect.width - 15;
+
+                GUI.Label(contentRect, Label.Text, EditorStyles.boldLabel);
+            }
+            else
+            {
+                contentRect.width = buttonRect.width - 10;
+
+                GUI.Label(contentRect, Label.Text, EditorStyles.boldLabel);
+            }
+            string status = _isOpen ? "－" : "＋";
+            contentRect.x += contentRect.width;
+            contentRect.x -= 20;
+            contentRect.width = 20;
+            GUI.Label(contentRect, status, EditorStyles.boldLabel);
+
+            if (GUI.Button(buttonRect, new GUIContent(string.Empty, Label.Tooltip), style))
+            {
+                _isOpen = !_isOpen;
+                _fade.valueChanged.RemoveAllListeners();
+                _fade.valueChanged.AddListener(() =>
+                {
+                    if (EditorWindow.focusedWindow != null)
+                    {
+                        EditorWindow.focusedWindow.Repaint();
+                    }
+                });
+            }
+        }
+        private void DrawContent()
+        {
             foreach (var entry in Elements)
             {
                 if (entry is CategoryElement)
@@ -46,5 +99,7 @@ namespace Mystic
         {
             return Label.Text;
         }
+        AnimBool _fade = new AnimBool();
+        bool _isOpen = true;
     }
 }
