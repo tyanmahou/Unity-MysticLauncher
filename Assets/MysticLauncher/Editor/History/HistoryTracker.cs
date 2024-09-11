10f9cc8e2cd579b4f485767bab19ffaf9d4e84a7
tyanmahou <tyanmahou@gmail.com>
2024-09-11 15:58:55 +0900

    update HistoryTracker

--- Assets/MysticLauncher/Editor/History/HistoryTracker.cs
+++ Assets/MysticLauncher/Editor/History/HistoryTracker.cs
@@ -1,18 +1,36 @@
 ﻿using UnityEditor;
 using UnityEditor.Callbacks;
+using UnityEditor.SceneManagement;
 
 namespace Mystic
 {
+    [InitializeOnLoad]
     public static class HistoryTracker
     {
-        [OnOpenAsset]
+        static HistoryTracker()
+        {
+            PrefabStage.prefabStageOpened += OnPrefabOpened;
+        }
+        [OnOpenAsset(0)]
         public static bool OnOpen(int instanceID, int line)
         {
             var asset = EditorUtility.InstanceIDToObject(instanceID);
-
-            UserHistories.instance.Register(asset);
-            UserHistories.instance.Save();
+            Register(asset);
             return false;
+        }
+        private static void OnPrefabOpened(PrefabStage stage)
+        {
+            UnityEngine.GameObject prefabAsset = AssetDatabase.LoadAssetAtPath<UnityEngine.GameObject>(stage.assetPath);
+            Register(prefabAsset);
+        }
+
+        private static void Register(UnityEngine.Object obj)
+        {
+            if (obj != null)
+            {
+                UserHistories.instance.Register(obj);
+                UserHistories.instance.Save();
+            }
         }
     }
 }
