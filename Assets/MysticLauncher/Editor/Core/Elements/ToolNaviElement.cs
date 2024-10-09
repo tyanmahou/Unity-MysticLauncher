b30e3ca7a13a69c370786cc0ef203253ff20076d
tyanmahou <tyanmahou@gmail.com>
2024-10-10 04:40:07 +0900

    refactor navi scroll

--- Assets/MysticLauncher/Editor/Core/Elements/ToolNaviElement.cs
+++ Assets/MysticLauncher/Editor/Core/Elements/ToolNaviElement.cs
@@ -17,51 +17,8 @@ namespace Mystic
             _prevTime = (float)EditorApplication.timeSinceStartup;
 
             float minWidth = Elements.Length * 60 + (Elements.Length + 1) * 3;
-            float viewWidth = EditorGUIUtility.currentViewWidth;
-            float scrollSpeed = 60 * 20;
-            bool isScroll = minWidth > viewWidth;
 
-            var scrollStyle = new GUIStyle(GUIStyle.none);
-            scrollStyle.alignment = TextAnchor.MiddleCenter;
-            scrollStyle.normal.textColor = Color.white;
-
-            var y = GUILayoutUtility.GetRect(0, 0).y;
-            var leftScroll = new Rect(0, y, 20, 64);
-            var rightScroll = new Rect(viewWidth - 20, y, 20, 64);
-            bool useLeftScroll = isScroll && _scrollPosition.x > 0;
-            bool useRightScroll = isScroll && _scrollPosition.x < minWidth - viewWidth;
-
-            bool hoverLeft = false;
-            bool hoverRight = false;
-            using (new EditorGUI.DisabledGroupScope(!useLeftScroll))
-            {
-                if (GUI.RepeatButton(leftScroll, GUIContent.none, EditorStyles.iconButton))
-                {
-                    _scrollPosition.x -= scrollSpeed * deltaTime;
-                    _scrollPosition.x = Mathf.Max(0, _scrollPosition.x);
-                    EditorWindow.focusedWindow.Repaint();
-                    hoverLeft = true;
-                }
-                else if (leftScroll.Contains(Event.current.mousePosition))
-                {
-                    hoverLeft = true;
-                }
-            }
-            using (new EditorGUI.DisabledGroupScope(!useRightScroll))
-            {
-                if (GUI.RepeatButton(rightScroll, GUIContent.none, EditorStyles.iconButton))
-                {
-                    _scrollPosition.x += scrollSpeed * deltaTime;
-                    _scrollPosition.x = Mathf.Min(minWidth - viewWidth, _scrollPosition.x);
-                    EditorWindow.focusedWindow.Repaint();
-                    hoverRight = true;
-                }
-                else if (rightScroll.Contains(Event.current.mousePosition))
-                {
-                    hoverRight = true;
-                }
-            }
-            using (var scrollView = new GUILayout.ScrollViewScope(_scrollPosition, GUIStyle.none, GUIStyle.none, GUILayout.MinHeight(64), GUILayout.ExpandHeight(false)))
+            using (var scrollView = new SimpleHorizontalScrollScope(_scrollPosition, minWidth, height: 64, deltaTime, scrollSpeed: 60 * 20))
             {
                 using (new GUILayout.HorizontalScope())
                 {
@@ -74,18 +31,6 @@ namespace Mystic
                     }
                 }
                 _scrollPosition = scrollView.scrollPosition;
-            }
-            Color colorDefault = new Color(0.2196f, 0.2196f, 0.2196f, 1.0f);
-            Color colorHover = new Color(0.2196f, 0.2196f, 0.2196f, 0.8f);
-            if (useLeftScroll)
-            {
-                EditorGUI.DrawRect(leftScroll, hoverLeft ? colorHover : colorDefault);
-                GUI.Label(leftScroll, EditorGUIUtility.IconContent("d_back"), scrollStyle);
-            }
-            if (useRightScroll)
-            {
-                EditorGUI.DrawRect(rightScroll, hoverRight ? colorHover : colorDefault);
-                GUI.Label(rightScroll, EditorGUIUtility.IconContent("d_forward"), scrollStyle);
             }
         }
         public override string ToString()
---
+++ Assets/MysticLauncher/Editor/Utilities/SimpleHorizontalScrollScope.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Mystic
+{
+    public class SimpleHorizontalScrollScope : IDisposable
+    {
+        public SimpleHorizontalScrollScope(in Vector2 scrollPos, float contentWidth, float height, float deltaTime, float scrollSpeed = 1200) 
+        {
+            this.scrollPosition = scrollPos;
+
+            float viewWidth = EditorGUIUtility.currentViewWidth;
+            bool isScroll = contentWidth > viewWidth;
+
+            _scrollStyle = new GUIStyle(GUIStyle.none);
+            _scrollStyle.alignment = TextAnchor.MiddleCenter;
+            _scrollStyle.normal.textColor = Color.white;
+
+            var y = GUILayoutUtility.GetRect(0, 0).y;
+            _leftScroll = new Rect(0, y, 20, height);
+            _rightScroll = new Rect(viewWidth - 20, y, 20, height);
+            _useLeftScroll = isScroll && scrollPosition.x > 0;
+            _useRightScroll = isScroll && scrollPosition.x < contentWidth - viewWidth;
+
+            _hoverLeft = false;
+            _hoverRight = false;
+            using (new EditorGUI.DisabledGroupScope(!_useLeftScroll))
+            {
+                if (GUI.RepeatButton(_leftScroll, GUIContent.none, EditorStyles.iconButton))
+                {
+                    scrollPosition.x -= scrollSpeed * deltaTime;
+                    scrollPosition.x = Mathf.Max(0, scrollPosition.x);
+                    EditorWindow.focusedWindow.Repaint();
+                    _hoverLeft = true;
+                }
+                else if (_leftScroll.Contains(Event.current.mousePosition))
+                {
+                    _hoverLeft = true;
+                }
+            }
+            using (new EditorGUI.DisabledGroupScope(!_useRightScroll))
+            {
+                if (GUI.RepeatButton(_rightScroll, GUIContent.none, EditorStyles.iconButton))
+                {
+                    scrollPosition.x += scrollSpeed * deltaTime;
+                    scrollPosition.x = Mathf.Min(contentWidth - viewWidth, scrollPosition.x);
+                    EditorWindow.focusedWindow.Repaint();
+                    _hoverRight = true;
+                }
+                else if (_rightScroll.Contains(Event.current.mousePosition))
+                {
+                    _hoverRight = true;
+                }
+            }
+            _scrollView = new GUILayout.ScrollViewScope(scrollPosition, GUIStyle.none, GUIStyle.none, GUILayout.MinHeight(height), GUILayout.ExpandHeight(false));
+        }
+
+        public void Dispose() 
+        {
+            scrollPosition = _scrollView.scrollPosition;
+            _scrollView.Dispose();
+            Color colorDefault = new Color(0.2196f, 0.2196f, 0.2196f, 1.0f);
+            Color colorHover = new Color(0.2196f, 0.2196f, 0.2196f, 0.8f);
+            if (_useLeftScroll)
+            {
+                EditorGUI.DrawRect(_leftScroll, _hoverLeft ? colorHover : colorDefault);
+                GUI.Label(_leftScroll, EditorGUIUtility.IconContent("d_back"), _scrollStyle);
+            }
+            if (_useRightScroll)
+            {
+                EditorGUI.DrawRect(_rightScroll, _hoverRight ? colorHover : colorDefault);
+                GUI.Label(_rightScroll, EditorGUIUtility.IconContent("d_forward"), _scrollStyle);
+            }
+        }
+        public Vector2 scrollPosition;
+
+        GUILayout.ScrollViewScope _scrollView;
+        GUIStyle _scrollStyle;
+        Rect _leftScroll;
+        Rect _rightScroll;
+        bool _hoverLeft = false;
+        bool _hoverRight = false;
+        bool _useLeftScroll = false;
+        bool _useRightScroll = false;
+    }
+}
