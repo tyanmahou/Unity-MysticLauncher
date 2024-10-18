d0118d57041da30da8365af836fc68d105dc531d
tyanmahou <tyanmahou@gmail.com>
2024-10-19 02:10:15 +0900

    add ScrollView

---
+++ Assets/MysticLauncher/Editor/Core/Elements/ScrollViewElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Mystic
+{
+    [Serializable]
+    public class ScrollViewElement : IElement
+    {
+        public float Height = 0;
+        public bool ExpandHeight = false;
+
+        [NamedArrayElement, SerializeReference, SubclassSelector]
+        public IElement[] Elements;
+
+        public void OnGUI()
+        {
+            using var scroller = new EditorGUILayout.ScrollViewScope(_scrollPosition, GUILayout.Height(Height), GUILayout.ExpandHeight(ExpandHeight));
+            _scrollPosition = scroller.scrollPosition;
+
+            foreach (var entry in Elements)
+            {
+                entry?.OnGUI();
+            }
+        }
+        public override string ToString()
+        {
+            return "Scroll";
+        }
+        Vector2 _scrollPosition;
+    }
+}
