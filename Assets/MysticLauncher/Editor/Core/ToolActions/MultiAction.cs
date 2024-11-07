8283115b00bc519ad2d1db3758ed2c72299f8135
tyanmahou <tyanmahou@gmail.com>
2024-11-07 23:11:54 +0900

    add multi action

---
+++ Assets/MysticLauncher/Editor/Core/ToolActions/MultiAction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Mystic
+{
+    [Serializable]
+    public class MultiAction : IToolAction
+    {
+        [SerializeReference, SubclassSelector]
+        public IToolAction[] Actions = new IToolAction[0];
+
+        public void Execute()
+        {
+            foreach (var action in Actions) 
+            {
+                action?.Execute();
+            }
+        }
+        public string Tooltip()
+        {
+            return string.Join(Environment.NewLine, Actions.Select(a => a?.Tooltip()).Where(s => !string.IsNullOrEmpty(s)));
+        }
+    }
+}
