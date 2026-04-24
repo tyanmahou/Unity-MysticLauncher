c16a9bc028155417ceb5a792366f07d6472b8b0f
tyanmahou <tyanmahou@gmail.com>
2026-04-25 01:34:25 +0900

    add template element

---
+++ Assets/MysticLauncher/Editor/Core/Elements/TemplateElement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Mystic
+{
+    [Serializable]
+    public class TemplateElement : IElement
+    {
+        [SerializeField] TemplateElementAsset _template;
+
+        public TemplateElement() { }
+        public TemplateElement(TemplateElementAsset template)
+        {
+            _template = template;
+        }
+
+        public void OnGUI()
+        {
+            if (_template == null)
+            {
+                return;
+            }
+            foreach (var entry in _template.Elements)
+            {
+                entry?.OnGUI();
+            }
+        }
+        public override string ToString()
+        {
+            return _template == null ? "None Template" : _template.name;
+        }
+    }
+}
---
+++ Assets/MysticLauncher/Editor/Core/Elements/TemplateElementAsset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Mystic
+{
+    [CreateAssetMenu(fileName = "NewTemplateElement", menuName = "Mystic/TemplateElementAsset", order=99998)]
+    public class TemplateElementAsset : ScriptableObject
+    {
+        [NamedArrayElement, SerializeReference, SubclassSelector]
+        public IElement[] Elements;
+    }
+}
